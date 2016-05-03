b2b97f6c7fbd0929c88ff2f595f226e10880d616
Rafael Santos Salustriano <rafaelss5040@gmail.com>
2016-05-03 00:14:29 -0300

    Add Código

---
+++ AlgoritmoLogicaDeProgramacaoII/src/Exercicio51.java
@@ -0,0 +1,27 @@
+import javax.swing.JFrame;
+
+
+public class Exercicio51  {
+
+
+	public static void main(String[] args) {
+		janela();
+	}
+	
+	
+	static void janela(){
+		JFrame janela = new JFrame();
+		janela.setTitle("Sistema de Cadastro ");
+		janela.setSize(700,500);
+		janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		janela.setLocationRelativeTo(null);
+		janela.setResizable(false);
+		janela.setVisible(true);
+	
+		
+	}
+	
+	
+	
+	
+}
